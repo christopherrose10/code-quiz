84fa21bc53d1a0086941296075a1ec1c47707647
UA CR <blazer5@email.arizona.edu>
2021-05-23 23:57:58 -0700

    timer added in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,18 @@
 var timerEl = document.getElementById('countdown');
+var mainEl = document.getElementById('main');
+var startBtn = document.getElementById('start');
+
+function countdown() {
+    var timeLeft = 90;
+
+
+    var timeInterval = setInterval(function () {
+        timerEl.textContent = "Time: " + timeLeft;
+        timeLeft --;
+    }, 1000);
+
+
+}
+
+startBtn.onclick = countdown;
+
